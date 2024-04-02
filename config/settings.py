bcef7e69d7795b1f92e69a349d11edfaaa0fd9bc
ABROR-build <142251145+ABROR-build@users.noreply.github.com>
2024-04-02 11:06:42 +0500

    render commit2

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY = "django-insecure-k5ccet^vm_j#uf$@uobedlnuhp8zii)l)k&j^6vq%6^5!&tuh("
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
